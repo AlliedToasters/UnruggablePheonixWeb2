618eb1bfe94c2b100929d3b07c20b8bafeb0517d
michael <michael.r.klear@gmail.com>
2021-11-24 18:10:31 -0800

    add code

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask
+from flask import Flask, request
+import json
+with open("./metadata.json", "r") as f:
+    md = json.loads(f.read())
+
+def get_metadata(id, md=md):
+    if not 0 < id < 889:
+        return json.dumps({"error":"id is not between 1 and 888"})
+    else:
+        return json.dumps(md[str(id)])
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello Sammy!'
+@app.route('/healthcheck')
+def healthcheck():
+    return 'Healthy!'
+
+@app.route('/metadata')
+def metadata():
+    id = request.args.get('id')
+    return get_metadata(id)
